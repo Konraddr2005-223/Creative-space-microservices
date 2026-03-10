21d3bbf863aec172f06f024239c02da11f5d4776
Konrad <konraddr2005@wp.pl>
2026-03-10 23:56:18 +0100

    init: dodano serwer Eureka (Service Discovery)

---
+++ service-discovery/service-discovery/src/main/java/pl/creativespace/service_discovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package pl.creativespace.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // 1. Import
+
+@SpringBootApplication
+@EnableEurekaServer // 2. Dodaj tę adnotację
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
